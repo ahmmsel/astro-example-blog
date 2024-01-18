a0eabda65608734db1c24e473f8c27843b5099d6
Ali <ahmmsel2@gmail.com>
2024-01-18 18:05:02 +0200

    add files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
-  adapter: vercel()
+  output: 'static',
+  adapter: vercelStatic(),
 });
