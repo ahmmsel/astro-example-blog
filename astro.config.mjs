9996d0faaac7fa76db8cbe4eabe01875891d3add
Ali <ahmmsel2@gmail.com>
2024-01-26 03:04:01 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel()
 });
